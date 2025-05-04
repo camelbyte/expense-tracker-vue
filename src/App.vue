cb8b92231db6a990905e52082baa0f4e6f10f9d5
xWhaler <camelbyte@proton.me>
2025-05-04 08:09:24 -0400

    added files

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <Header style="text-align: center" /> 
+</template>
+
+
+<script>
+import Header from './components/Header.vue';
+
+export default {
+    
+    components: {
+        Header,
+
+    }
+
+}
+</script>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,8 @@
+<template>
+    <h3 class="text-center">
+        <span class="text-2xl font-bold">Transaction List</span>
+        <br />
+        <span class="text-sm text-gray-500">Here you can see all your transactions</span>
+        
+    </h3>
+</template>
